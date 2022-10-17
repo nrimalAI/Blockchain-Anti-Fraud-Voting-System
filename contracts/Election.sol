628ef938b584297a014d32ca4fe3ab6671104f6c
Nitant R <54756510+nrimalAI@users.noreply.github.com>
2022-10-17 16:55:00 -0400

    initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Voting{
+    
+    string public candidate;
+    
+    //Constructor
+
+     constructor(){
+        candidate = "Candidate 1";
+     }
+}
+
